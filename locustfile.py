f2b1d2f8037d94118da1a12ca13c326c2ef405a0
Zen <zen.nguyen@moodys.com>
2021-08-11 21:14:37 -0700

    first commit

---
+++ locustfile.py
@@ -0,0 +1,10 @@
+from locust import HttpUser, task
+
+class HelloWorldUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.get("https://api-microservices.sfportal.com/_/healthz")
+#        self.client.get("https://api-microservices.sfportal.com/_/readyz")
+#        self.client.get("http://a661777e4b52f4ea2ad90bb6392cce0e-9064b8fda52262ca.elb.us-west-2.amazonaws.com/_/readyz")
+#        self.client.get("http://a661777e4b52f4ea2ad90bb6392cce0e-9064b8fda52262ca.elb.us-west-2.amazonaws.com/_/healthz")
+
